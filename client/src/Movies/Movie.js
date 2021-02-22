728b24024fb7c456255de5f7bb99d442d454c1ac
Nicholas Smith <nicholassmithbus@gmail.com>
2021-02-22 15:48:50 -0700

    Refactored code in Movie.js MovieList.js into MovieCard.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom'
+import MovieCard from './MovieCard'
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
@@ -30,27 +31,5 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+  return <MovieCard movie={movie} />
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { useHistory, useRouteMatch } from 'react-router-dom';
+import MovieCard from './MovieCard'
 
 export default function MovieList(props) {
   
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie}  />
+        <MovieCard key={movie.id} movie={movie}  />
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore, id } = props.movie;
-  const history = useHistory();
-  const { url } = useRouteMatch();
-  return (
-    <div className="movie-card" onClick={() => history.push(`${url}movies/${id}`)}>
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
-  );
-}
